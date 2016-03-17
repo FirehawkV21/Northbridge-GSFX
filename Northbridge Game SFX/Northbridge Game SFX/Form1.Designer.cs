6de5731d0c448cf6ffc0e2b08595ccc9052746ba
AceOfAces <acemod13@outlook.com>
2016-03-17 14:06:44 +0200

    Set up foundation.

---
+++ Northbridge Game SFX/Northbridge Game SFX/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Northbridge_Game_SFX
+{
+    partial class BaseForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BaseForm));
+            this.ExtractButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.AutoLaunchButton = new System.Windows.Forms.CheckBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.GamePathBox = new System.Windows.Forms.TextBox();
+            this.GameVersionLabel = new System.Windows.Forms.Label();
+            this.GameExtractPicker = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // ExtractButton
+            // 
+            resources.ApplyResources(this.ExtractButton, "ExtractButton");
+            this.ExtractButton.Name = "ExtractButton";
+            this.ExtractButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            resources.ApplyResources(this.ExitButton, "ExitButton");
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // AutoLaunchButton
+            // 
+            resources.ApplyResources(this.AutoLaunchButton, "AutoLaunchButton");
+            this.AutoLaunchButton.Name = "AutoLaunchButton";
+            this.AutoLaunchButton.UseVisualStyleBackColor = true;
+            // 
+            // BrowseButton
+            // 
+            resources.ApplyResources(this.BrowseButton, "BrowseButton");
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // GamePathBox
+            // 
+            resources.ApplyResources(this.GamePathBox, "GamePathBox");
+            this.GamePathBox.Name = "GamePathBox";
+            this.GamePathBox.TextChanged += new System.EventHandler(this.GamePathBox_TextChanged);
+            // 
+            // GameVersionLabel
+            // 
+            resources.ApplyResources(this.GameVersionLabel, "GameVersionLabel");
+            this.GameVersionLabel.Name = "GameVersionLabel";
+            // 
+            // GameExtractPicker
+            // 
+            resources.ApplyResources(this.GameExtractPicker, "GameExtractPicker");
+            // 
+            // BaseForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.GameVersionLabel);
+            this.Controls.Add(this.GamePathBox);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.AutoLaunchButton);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ExtractButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "BaseForm";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Load += new System.EventHandler(this.BaseForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ExtractButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.CheckBox AutoLaunchButton;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.TextBox GamePathBox;
+        private System.Windows.Forms.Label GameVersionLabel;
+        private System.Windows.Forms.FolderBrowserDialog GameExtractPicker;
+    }
+}
+
---
+++ Northbridge Game SFX/Northbridge Game SFX/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Northbridge_Game_SFX
+{
+    public partial class BaseForm : Form
+    {
+        //The game's name is placed here.
+        private static string GameName = "Example Game";
+        //The default path to extract the game files.
+        private static string GameXtractPath = Application.StartupPath + "\\" + GameName + "\\";
+        public BaseForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BrowseButton_Click(object sender, EventArgs e)
+        {
+            var dl = GameExtractPicker.ShowDialog();
+            if (dl != DialogResult.OK) return;
+            GameXtractPath = GameExtractPicker.SelectedPath;
+            GamePathBox.Text = GameXtractPath;
+        }
+
+        private void GamePathBox_TextChanged(object sender, EventArgs e)
+        {
+            GameXtractPath = GamePathBox.Text;
+        }
+
+        private void BaseForm_Load(object sender, EventArgs e)
+        {
+            GamePathBox.Text = GameXtractPath;
+        }
+    }
+}
